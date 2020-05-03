1cae35f74e8f95e7dfd79e693dc25db4ffc77499
daisu8e <katsumata.coffee.150yen@gmail.com>
2020-05-03 12:29:27 -0700

    changed the backend bucket name

--- staging/main.tf
+++ staging/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "= 0.12.18"
   backend "s3" {
-    bucket = "portfolio.daisu8e.com.tfstates"
+    bucket = "portfolio.daisu8e.com.tfstates.old"
     key = "staging/terraform.tfstate"
     region = "us-east-1"
   }
