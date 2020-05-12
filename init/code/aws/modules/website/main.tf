c952fb3b64bf21285b7fa0bc2e01b61e1c6b9a96
daisu8e <katsumata.coffee.150yen@gmail.com>
2020-05-12 11:35:59 -0700

    added hosted_zone_id to result

--- init/code/aws/modules/website/main.tf
+++ init/code/aws/modules/website/main.tf
@@ -9,6 +9,7 @@ ${path.module} = {
   }
   cloudfront = {
     distribution = ${aws_cloudfront_distribution.this.id}
+    hosted_zone_id = ${aws_cloudfront_distribution.this.hosted_zone_id}
     domain_name = ${aws_cloudfront_distribution.this.domain_name}
   }
 }
